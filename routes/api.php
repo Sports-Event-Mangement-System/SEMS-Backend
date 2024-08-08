7012ed4ffe1f51a069e373ed4b20c3d36b77baca
Aayushkalikote <aayushkalikote31@gmail.com>
2024-08-08 15:57:21 +0545

    contact us mvc done and store api has been created with valdiation

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum' );
+
+/************************************************************** Contacts Form *************************************************************/
+Route::post('/store/contacts', [ContactController::class, 'store']);
+// Route::post('/delete/contacts', [AuthController::class, 'destroy']);
+
