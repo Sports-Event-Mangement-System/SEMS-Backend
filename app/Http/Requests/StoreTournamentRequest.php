8e12dc907f70d00217c1d6fbcda1df5464908eb6
Aayushkalikote <aayushkalikote31@gmail.com>
2024-08-29 21:25:05 +0545

    added tournament store logic, database related to it, validation support and model as it required

---
+++ app/Http/Requests/StoreTournamentRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTournamentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            't_name' => 'required',
+            't_description' => 'required',
+            't_logo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Added image validation
+            't_type' => 'nullable',
+            'ts_date' => 'required|date',
+            'te_date' => 'required|date',
+            'rs_date' => 'required|date',
+            're_date' => 'required|date',
+            'phone_number' => 'required',
+            'email' => 'nullable|email',
+            'address' => 'required',
+            'status' => 'required|boolean',
+            'team_number' => 'required|integer',
+            'featured' => 'required|boolean',
+        ];
+    }
+}
--- database/migrations/2024_08_08_091627_create_tournaments_table.php
+++ database/migrations/2024_08_08_091627_create_tournaments_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('tournaments', function (Blueprint $table) {
             $table->id();
+            $table->string('t_name');
+            $table->string('t_description');
+            $table->string('t_logo');
+            $table->enum('t_type',['indoor','outdoor'])->default('outdoor');
+            $table->timestamps('ts_date');
+            $table->timestamps('te_date');
+            $table->timestamps('rs_date');
+            $table->timestamps('re_date');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('address');
+            $table->boolean('status');
+            $table->string('team_number');
+            $table->boolean('featured');
             $table->timestamps();
         });
     }
