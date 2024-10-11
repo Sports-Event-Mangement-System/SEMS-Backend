31ece4a847feab3f243cb6cb7496e8ae8eea32f6
Aayushkalikote <aayushkalikote31@gmail.com>
2024-10-11 21:32:05 +0545

    Update and Delete Team APi for frontend with proper logic

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Helper\EmailHelper;
 use App\Helper\ImageHelper;
 use App\Http\Requests\StoreTeamRequest;
+use App\Http\Requests\UpdateTeamRequest;
 use App\Models\Player;
 use App\Models\Team;
 use Illuminate\Http\JsonResponse;
@@ -66,6 +67,59 @@ class TeamController extends Controller
             'status' => true,
         ]);
     }
+    public function update(UpdateTeamRequest $request, $id): JsonResponse
+    {
+        $validatedData = $request->validated();
+        $team = Team::find($id);
+
+        if ($request->hasFile('team_logo')) {
+            if (!empty($team->team_logo) && file_exists(public_path('uploads/teams/' . $team->team_logo))) {
+            unlink(public_path('uploads/teams/' . $team->team_logo));
+            }
+            $file = $request->file('team_logo');
+            $filename = time().'_'.$file->getClientOriginalName();
+            $file->move(public_path('uploads/teams'), $filename);
+        } elseif (!empty($request->existing_images)) {
+            $filename = basename($request->existing_images);
+        }
+
+        $team->update([
+            'tournament_id' => $validatedData['tournament_id'],
+            'team_name' => $validatedData['team_name'],
+            'team_logo' => $filename,
+            'coach_name' => $validatedData['coach_name'],
+            'phone_number' => $validatedData['phone_number'],
+            'email' => $validatedData['email'],
+            'address' => $validatedData['address'],
+            'player_number' => $validatedData['player_number'],
+        ]);
+        $players = $request->players;
+        foreach ($players as $key => $player) {
+            if( !empty($player['player_id'] )) {
+                $playerfind = Player::findOrFail($player['player_id']);
+                $playerfind->update([
+                    'team_id' => $team->id,
+                    'player_name' => $player['player_name'],
+                    'player_email' => $player['player_email'],
+                    'is_captain' => $key === 0 ? 1 : 0,
+                ]);
+            }else{
+                $player_save = Player::create([
+                    'team_id' => $team->id,
+                    'player_name' => $player['player_name'],
+                    'player_email' => $player['player_email'],
+                    'is_captain' => $key === 0 ? 1 : 0,
+                ]);
+                $player_save->save();
+            }
+        }
+
+        return response()->json([
+            'message' => 'Team Updated successfully',
+            'team' => $team,
+            'status' => true,
+        ]);
+    }
 
     public function updateStatus(Request $request, $id): JsonResponse
     {
@@ -93,6 +147,19 @@ class TeamController extends Controller
 
         return response()->json([
             'team' => $team,
+            'status' => true,
+        ]);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $team = Team::find($id);
+        if ($team) {
+            Player::where('team_id', $id)->delete();
+            $team->delete();
+        }
+        return response()->json([
+            'message' => 'Team and associated players deleted successfully',
             'status' => true,
         ]);
     }
