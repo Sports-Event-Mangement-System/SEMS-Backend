7f5c50f2a7447a1eae4a9cc65f522fdf6269d5e6
Aayushkalikote <aayushkalikote31@gmail.com>
2024-12-01 21:08:14 +0545

    Sitesettings api, model controller and tables and logic for udpate and fetch site settings

---
+++ database/migrations/2024_12_01_144942_create_site_settings_table.php.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('site_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('site_name');
+            $table->string('site_description');
+            $table->string('site_logo');
+            $table->string('site_favicon');
+            $table->string('site_email');
+            $table->string('mail_mailer');
+            $table->string('mail_host');
+            $table->string('mail_port');
+            $table->string('mail_username');
+            $table->string('mail_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site_settings');
+    }
+};
