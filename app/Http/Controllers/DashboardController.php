af8f0d1be24841d0460dd7939bcf9d2a942a0c84
Aayushkalikote <aayushkalikote31@gmail.com>
2024-12-07 21:39:48 +0545

    Fix: Query of Dashboard and Site Setting for email settings.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -22,25 +22,25 @@ class DashboardController extends Controller
         $upcoming_tournaments = $tournaments->where('ts_date', '>', now())->count();
         $completed_tournaments = $tournaments->where('te_date', '<', now())->count();
 
-        $starting_this_month_tournaments = $tournaments->where('ts_date', '>=', now()->startOfMonth())->first();
+        $starting_this_month_tournaments = $tournaments->where('ts_date', '>=', now()->startOfMonth());
         foreach ($starting_this_month_tournaments as $starting_this_month_tournament) {
-            $tournament_name = $starting_this_month_tournament->tournament_name;
+            $tournament_name = $starting_this_month_tournament->t_name;
             $st_tournmanet_this_month = array(
                 'event_name' => $tournament_name .'Tourament Starting this month',
                 'event_date' => $starting_this_month_tournament->ts_date,
             );
         }
-        $registrations_started_this_month = $tournaments->where('ts_date', '>=', now()->startOfMonth())->first();
+        $registrations_started_this_month = $tournaments->where('rs_date', '>=', now()->startOfMonth());
         foreach ($registrations_started_this_month as $registration_starting_this_month) {
-            $tournament_name = $registration_starting_this_month->tournament_name;
+            $tournament_name = $registration_starting_this_month->t_name;
             $rs_tournmanet_this_month = array(
                 'event_name' => $tournament_name .'Tournament Registrations started',
                 'event_date' => $registration_starting_this_month->rs_date,
             );
         }
-        $registrations_ended_this_month = $tournaments->where('te_date', '<=', now()->endOfMonth())->first();
+        $registrations_ended_this_month = $tournaments->where('re_date', '<=', now()->endOfMonth());
         foreach ($registrations_ended_this_month as $registration_ended_this_month) {
-            $tournament_name = $registration_ended_this_month->tournament_name;
+            $tournament_name = $registration_ended_this_month->t_name;
             $re_tournmanet_this_month = array(
                 'event_name' => $tournament_name .'Tournament Registrations ended',
                 'event_date' => $registration_ended_this_month->re_date,
